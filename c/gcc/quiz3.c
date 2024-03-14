76cad43034701985d539a244668f84493f9a799e
songhannaa <shn0322@kakao.com>
2024-03-14 11:08:51 +0900

    quiz3

---
+++ c/gcc/quiz3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "libcheckprime.h"
+
+void main(){
+    while(1)
+    {
+        int n;
+        printf("Input integer (0:Exit) : ");
+        scanf("%d", &n);
+        if(n == 0)
+            break;
+        if(checkprime(n) == n)
+            printf("%d is prime number\n", n);
+        else
+            printf("%d is not prime number\n", n);
+    }
+}
